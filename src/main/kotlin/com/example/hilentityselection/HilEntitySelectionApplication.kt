ea1bf4bfe93e8128d1fc60fdd8a02fad92444daf
Varun Sharma <v.sharma@elsevier.com>
2021-01-31 13:42:12 +0530

    consumer

--- src/main/kotlin/com/example/hilentityselection/HilEntitySelectionApplication.kt
+++ src/main/kotlin/com/example/hilentityselection/HilEntitySelectionApplication.kt
@@ -1,47 +1,36 @@
 package com.example.hilentityselection
 
-import org.apache.avro.Schema
-import org.apache.avro.generic.GenericData
 import org.apache.avro.generic.GenericRecord
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.schema.registry.client.ConfluentSchemaRegistryClient
-import org.springframework.cloud.schema.registry.client.EnableSchemaRegistryClient
 import org.springframework.cloud.schema.registry.client.SchemaRegistryClient
 import org.springframework.context.annotation.Bean
 import reactor.core.publisher.Flux
-import reactor.util.function.Tuple2
-import reactor.util.function.Tuples
-import java.util.function.Function
+import java.util.function.Consumer
 
 
 @SpringBootApplication
-//@EnableSchemaRegistryClient
-class HilEntitySelectionApplication{
+class HilEntitySelectionApplication(val logger: Logger = LoggerFactory.getLogger(HilEntitySelectionApplication::class.java)) {
 
-	@Bean
-	fun singleInputMultipleOutputs(): Function<Flux<GenericRecord>, Tuple2<Flux<GenericRecord>, Flux<GenericRecord>>>? {
-		return Function { flux: Flux<GenericRecord> ->
-			val connectedFlux = flux.publish().autoConnect(2)
-			val passedFlux = connectedFlux.filter { it.get("score") as Int > 80 }
-			val failedFlux = connectedFlux.filter { it.get("score") as Int <= 80 }
-			print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-			Tuples.of(
-				passedFlux, failedFlux
-			)
-		}
-	}
+    @Bean
+    fun singleInputMultipleOutputs(): Consumer<Flux<GenericRecord>>? {
+        return Consumer { flux: Flux<GenericRecord> ->
 
-	@Bean
-	fun schemaRegistryClient(): SchemaRegistryClient {
-		val client = ConfluentSchemaRegistryClient()
-		client.setEndpoint("http://localhost:8081")
-		return client
-	}
+            flux.subscribe { logger.info("Record: $it \n Schema: ${it.schema}") }
+        }
+    }
 }
 
-
+@Bean
+fun schemaRegistryClient(): SchemaRegistryClient {
+	val client = ConfluentSchemaRegistryClient()
+	client.setEndpoint("http://localhost:8081")
+	return client
+}
 
 fun main(args: Array<String>) {
-	runApplication<HilEntitySelectionApplication>(*args)
+    runApplication<HilEntitySelectionApplication>(*args)
 }
