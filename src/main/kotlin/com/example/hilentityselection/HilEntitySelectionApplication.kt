701f4970c32215aa2b65c9efb53060cdaf5d2938
Varun Sharma <v.sharma@elsevier.com>
2021-01-31 17:53:50 +0530

    consume | filter | produce

--- src/main/kotlin/com/example/hilentityselection/HilEntitySelectionApplication.kt
+++ src/main/kotlin/com/example/hilentityselection/HilEntitySelectionApplication.kt
@@ -7,31 +7,44 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.schema.registry.client.ConfluentSchemaRegistryClient
-import org.springframework.cloud.schema.registry.client.SchemaRegistryClient
 import org.springframework.context.annotation.Bean
 import reactor.core.publisher.Flux
-import java.util.function.Supplier
+import reactor.util.function.Tuple2
+import reactor.util.function.Tuples
+import java.util.function.Function
 
-val schemaStr = "{\"type\": \"record\", \"name\": \"Entity\", \"fields\": [{\"name\": \"name\", \"type\": \"string\"}, {\"name\": \"score\", \"type\": \"int\"}]}"
+const val entitySchemaStr =
+    "{\"type\": \"record\", \"name\": \"HilEntity\", \"fields\": [{\"name\": \"reason\", \"type\": \"string\"}]}"
+
+val entitySchema: Schema = Schema.Parser().parse(entitySchemaStr)
 
 @SpringBootApplication
 class HilEntitySelectionApplication(val logger: Logger = LoggerFactory.getLogger(HilEntitySelectionApplication::class.java)) {
 
     @Bean
-    fun singleInputMultipleOutputs(): Supplier<Flux<GenericRecord>>? {
-        val record = GenericData.Record(Schema.parse(schemaStr))
-        record.put("score", 99)
-        record.put("name", "auth")
-        return Supplier { Flux.just(record)}
+    fun singleInputMultipleOutputs(): Function<Flux<GenericRecord>, Tuple2<Flux<GenericRecord>, Flux<GenericRecord>>> {
+        return Function { flux ->
+            val connectedFlux = flux.publish().autoConnect(2)
+
+
+            val passedEntities: Flux<GenericRecord> = connectedFlux
+                .filter { it.get("score") as Int > 80 }
+                .map { "Score more than 80".asHilEntityRecord() }
+
+            val failedEntities: Flux<GenericRecord> = connectedFlux
+                .filter { it.get("score") as Int <= 80 }
+                .map { "Score less than 80".asHilEntityRecord() }
+
+            Tuples.of(passedEntities, failedEntities)
+
+        }
     }
 }
 
-@Bean
-fun schemaRegistryClient(): SchemaRegistryClient {
-	val client = ConfluentSchemaRegistryClient()
-	client.setEndpoint("http://localhost:8081")
-	return client
+private fun String.asHilEntityRecord(): GenericData.Record {
+    val record = GenericData.Record(entitySchema)
+    record.put("reason", this)
+    return record
 }
 
 fun main(args: Array<String>) {
